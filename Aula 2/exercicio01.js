fe351e7ffa12e47c90943a0f49d9f569867657b4
CliutonMauro <cliutonmauro@gmail.com>
2025-10-16 21:08:11 -0300

    Commit das questões da segunda aula

---
+++ Aula 2/exercicio01.js
@@ -0,0 +1,13 @@
+let nome = prompt("Qual o seu nome ? ");
+let idade = prompt("Qual a sua idade ? ");
+let altura = prompt("Qual a sua altura ? ");
+let genero = prompt("Qual o seu gênero ? ");
+let estudante = prompt("Você é estudante ? ");
+
+alert(`Bem vindo ${nome}!
+Seu Perfil
+Nome: ${nome}
+Idade: ${idade}
+Altura: ${altura}
+Gênero: ${genero}
+Estudante: ${estudante}`);
---
+++ Aula 2/exercicio05.js
@@ -0,0 +1,3 @@
+let confirmacao = confirm("Deseja excluir a sua conta? Se sim, clique em OK. Se não, clique em Cancelar.");
+
+confirmacao ? alert("Conta excluida com sucesso") : alert("Ação cancelada pelo usuário");
